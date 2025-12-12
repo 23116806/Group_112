557dc674ee647c6d2b7998b66c0e9573804d9d00
23116806 <is24acc@herts.ac.uk>
2025-12-12 15:54:43 +0530

    Data Loading & Preparation
    Load dataset and create a histogram of salaries.

---
+++ Salary.r
@@ -0,0 +1,21 @@
+############################################
+# 1. Load dataset (base R)
+############################################
+install.packages("tidyverse")
+data <- read.csv("engineering_graduate_salary.csv")
+
+# Check first rows
+head(data)
+
+############################################
+# 2. Histogram of Salary
+############################################
+
+# Create histogram object
+p_hist <- hist(
+  data$Salary,
+  main = "Distribution of Engineering Graduate Salaries",
+  xlab = "Annual Salary (INR)",
+  col = "skyblue",
+  border = "black"
+)
